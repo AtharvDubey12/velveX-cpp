1a5848ad457b3454bc6dfb6c47282dba1d9475b1
Atharv Dubey <athdubey120904@gmail.com>
2025-05-13 19:05:11 +0530

    added parenthesis sanitization

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,14 @@ using namespace std;
 
 int main(){
     vector<string> test = {"Integ", "2", "y", "y"};
-    string x = expressionParser("Integ[2][Integ[1][x][x] + p][y] + yx");
-    cout<<x;
+    string x = expressionParser("( Integ[1][x][x] )");
+    
+    string output = "";  //output with sanitized parenthesis.
+    for (int i=0; i<x.length(); i++){
+        if(x[i]=='(') output+= "\\left(";
+        else if (x[i]==')') output+= "\\right)";
+        else output+= x[i];
+    }
+    cout<<output;
     return 0;
 }
