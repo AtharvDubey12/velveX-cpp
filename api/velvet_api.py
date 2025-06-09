3380b2ee71ae7fd88574f1298c0da602380481ed
Atharv Dubey <athdubey120904@gmail.com>
2025-06-09 21:54:10 +0530

    Added a python API to velveX

---
+++ api/velvet_api.py
@@ -0,0 +1,15 @@
+import subprocess
+import os
+
+curr_dir = os.path.dirname(__file__)
+
+def call_velvex(command, relative_path = ["..", "bin", "parser.exe"]):
+    path = ""
+    for index in range(len(relative_path)):
+        if index != len(relative_path)-1:
+            path+= relative_path[index] + "\\"
+        else: path+= relative_path[index]
+    compiler_path = os.path.abspath(os.path.join(curr_dir, path)) 
+    result = subprocess.run([compiler_path, command], capture_output = True, text = True)
+    return result.stdout
+
