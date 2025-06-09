23d60679216f64be925ec7cb82e174f211470d31
Atharv Dubey <athdubey120904@gmail.com>
2025-06-09 22:57:45 +0530

    updated api

--- api/velvet_api.py
+++ api/velvet_api.py
@@ -7,9 +7,10 @@ def call_velvex(command, relative_path = ["..", "bin", "parser.exe"]):
     path = ""
     for index in range(len(relative_path)):
         if index != len(relative_path)-1:
-            path+= relative_path[index] + "\\"
-        else: path+= relative_path[index]
+            path += relative_path[index] + "\\"
+        else: path += relative_path[index]
     compiler_path = os.path.abspath(os.path.join(curr_dir, path)) 
     result = subprocess.run([compiler_path, command], capture_output = True, text = True)
-    return result.stdout
+    return result.stdout if result.stdout else result.stderr
+
 
